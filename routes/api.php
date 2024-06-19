cc4c78187b99347d59a24cf4b210a02c5b18a56d
GSG9HUN <olahsandorlajos99@gmail.com>
2024-06-19 14:48:45 +0200

    Base path added to routes.

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,8 @@
 use App\Http\Controllers\SecretController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/secret/{hash}', [SecretController::class, 'getSecretByHash'])->where('hash', '.*');
-Route::post('/secret', [SecretController::class, 'addSecret']);
+Route::prefix('/v1')->group(function () {
+    Route::get('/secret/{hash}', [SecretController::class, 'getSecretByHash'])->where('hash', '.*');
+    Route::post('/secret', [SecretController::class, 'addSecret']);
+});
+
