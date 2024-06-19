64144128d37372ad29671ee25dce5a615b2b1ce4
GSG9HUN <olahsandorlajos99@gmail.com>
2024-06-19 16:49:48 +0200

    Routes moved from api.php to web.php

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,4 @@
 <?php
 
-use App\Http\Controllers\SecretController;
-use Illuminate\Support\Facades\Route;
 
-Route::prefix('/v1')->group(function () {
-    Route::get('/secret/{hash}', [SecretController::class, 'getSecretByHash'])->where('hash', '.*');
-    Route::post('/secret', [SecretController::class, 'addSecret']);
-});
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\SecretController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('/v1')->group(function () {
+    Route::get('/secret/{hash}', [SecretController::class, 'getSecretByHash'])->where('hash', '.*');
+    Route::post('/secret', [SecretController::class, 'addSecret']);
 });
